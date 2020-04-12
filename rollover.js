69da69c30e74d2d49aa710c641a41f12f70dcbe4
BellaSanathong <32287638+BellaSanathong@users.noreply.github.com>
2020-04-11 20:10:02 -0500

    Update rollover.js

--- rollover.js
+++ rollover.js
@@ -16,16 +16,16 @@ window.onload = function() {
 
     // attach mouseover and mouseout events for each image
     image1.onmouseover = function() {
-        image1.src = "images/release.jpg";
+        image1.src = "release.jpg";
     };
     image1.onmouseout = function() {
-        image1.src = "images/hero.jpg";
+        image1.src = "hero.jpg";
     };
     
     image2.onmouseover = function() {
-        image2.src = "images/deer.jpg";
+        image2.src = "deer.jpg";
     };
     image2.onmouseout = function() {
-        image2.src = "images/bison.jpg";
+        image2.src = "bison.jpg";
     };
 };
